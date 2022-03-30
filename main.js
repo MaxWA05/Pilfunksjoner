0c3063b1b4dddd8ca07c3224d6e493af0a9aeb11
MaxWA05 <max.asheim@gmail.com>
2022-03-30 09:36:40 +0200

    adding pilfunksjon file

---
+++ main.js
@@ -0,0 +1,37 @@
+//oppgave 1
+const biler = _ =>{
+    console.log("Hello World!");
+}
+
+biler();
+
+
+
+
+//oppgave 2
+const biller = bil =>{
+    console.log("Biller er " + bil);
+}
+
+biller("tøffinger.");
+
+//Når det bare er en parameter trenger man ikke sett parantes rundt parametere.
+
+
+
+//oppgave 3
+const stein = (param1, param2, param3) =>{
+    console.log("Tre tøffe ting er " + param1 + ", " + param2 + " og " + param3);
+}
+
+stein("biler", "stein", "yoghurt.");
+
+
+//Oppgave 4
+
+
+//oppgave 5
+//Pilfunksjoner blir ikke hoistet, men funksjoner blir det.
+//Pilfunksjoner er mer som variabler.
+
+
